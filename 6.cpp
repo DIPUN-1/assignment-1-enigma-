643519df119f83b51bd0500eb1e7cb859c8648ef
DIPUN-1 <148847985+DIPUN-1@users.noreply.github.com>
2024-03-24 08:50:48 +0530

    assignment 5 is submitted.

---
+++ 6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    float r,pi = 3.1415;
+    cout<<"enter radius";
+    cin>>r;
+    cout<<"diameter is :"<<2*r;
+    cout<<"curcumference is :"<<2*pi*r;
+    cout<<"area is :"<<pi*r*r;
+    return 0;
+}
