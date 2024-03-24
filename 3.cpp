f56647035f6448fcf01c4953981173d624a8ddae
DIPUN-1 <148847985+DIPUN-1@users.noreply.github.com>
2024-03-24 08:49:16 +0530

    assignment 2 is submitted.

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int x;
+    cin>>x;
+    if(x > 0)
+    cout<<"it is positive";
+    else if(x == 0)
+    cout<<"it is zero";
+    else
+    cout<<"it is negative";
+    return 0;
+}
